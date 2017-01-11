7de26f854d93946dbc0bcd1dff0710a3e7bcfef7
韩建邦 <hanjianbang@xinshiyun.com>
2017-01-11 17:44:14 +0800

    feature:更新页面结构

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import React, {Component} from "react";
+import React, {Component} from "react"
+import {Layout} from 'antd'
+const {Header, Footer, Sider, Content} = Layout;
+
 // import Header from "./common/Header";
 // import Footer from "./common/Footer";
 
@@ -6,11 +9,18 @@ class App extends Component {
 
     render() {
         return (
-            <div id="content">
-                {/*<Header/>*/}
+            <Layout>
+                <Header className="test1">头部随便写写|</Header>
                 <div className="body">{this.props.children}</div>
-                {/*<Footer/>*/}
-            </div>
+
+                <Footer className="test">Footer</Footer>
+
+            </Layout>
+            // <div id="content">
+            //     {/*<Header/>*/}
+            //     <div className="body">{this.props.children}</div>
+            //     {/*<Footer/>*/}
+            // </div>
         )
     }
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,47 +5,48 @@ const {Header, Footer, Sider, Content} = Layout;
 class Home extends Component {
     render() {
         return (
-            <Layout>
-                <Header className="test1">头部随便写写|</Header>
-                <Content style={{padding: '30px 50px'}}>
+            // <div></div>
+            // <Layout>
+            //     <Header className="test1">头部随便写写|</Header>
+            <Content style={{padding: '30px 50px'}}>
+                <div>
+                    中间部分容器
+                    {/*每一个大块*/}
                     <div>
-                        中间部分容器
-                        {/*每一个大块*/}
                         <div>
-                            <div>
-                                <h1>大标题(公共资源)</h1>
-                            </div>
-                            <ul>
-                                <li>公共组件</li>
-                                <li>公共服务</li>
-                                <li>模板。。</li>
-                            </ul>
+                            <h1>大标题(公共资源)</h1>
                         </div>
-                        <div>
-                            <div>
-                                <h1>大标题(公共资源)</h1>
-                            </div>
-                            <ul>
-                                <li>公共组件</li>
-                                <li>公共服务</li>
-                                <li>模板。。</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <div>
-                                <h1>大标题(公共资源)</h1>
-                            </div>
-                            <ul>
-                                <li>公共组件</li>
-                                <li>公共服务</li>
-                                <li>模板。。</li>
-                            </ul>
-                        </div>
-
+                        <ul>
+                            <li>公共组件</li>
+                            <li>公共服务</li>
+                            <li>模板。。</li>
+                        </ul>
                     </div>
-                </Content>
-                <Footer className="test">Footer</Footer>
-            </Layout>
+                    <div>
+                        <div>
+                            <h1>大标题(公共资源)</h1>
+                        </div>
+                        <ul>
+                            <li>公共组件</li>
+                            <li>公共服务</li>
+                            <li>模板。。</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <div>
+                            <h1>大标题(公共资源)</h1>
+                        </div>
+                        <ul>
+                            <li>公共组件</li>
+                            <li>公共服务</li>
+                            <li>模板。。</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </Content>
+            //     <Footer className="test">Footer</Footer>
+            // </Layout>
         )
     }
 }
