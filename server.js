805ccbecd9a9247da03b03c5805412ee2a1381a8
alan <huanggy121@163.com>
2017-01-13 11:08:01 +0800

    add json

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 var http = require('http').createServer(app);
 var port = process.env.PORT || 8080;
+
+app.get('/data/:mode/:id', function (req, res) {
+    var fileName =  __dirname + "/data/" + req.params.mode + "/" + req.params.id + ".json";
+    fs.readFile(fileName, 'utf8', function (err, data) {
+        res.end( data );
+    });
+})
 
 app.use(express.static(__dirname));
 
