12d8de120e0d854cbc5f115a384b04725ff0836c
alan <huanggy121@163.com>
2017-03-13 22:51:07 +0800

    add getUserINfo cache

--- open-platform-backend/src/main/java/com/kyee/openplatform/controller/UserController.java
+++ open-platform-backend/src/main/java/com/kyee/openplatform/controller/UserController.java
@@ -1,28 +1,21 @@
 package com.kyee.openplatform.controller;
 
-import com.google.gson.Gson;
 import com.kyee.openplatform.config.web.ResultApi;
-import com.kyee.openplatform.pojo.HrpQueryResult;
 import com.kyee.openplatform.repositorys.user.UserInfo;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.util.LinkedMultiValueMap;
+import com.kyee.openplatform.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Map;
 
 @RestController
 public class UserController {
 
-    @Value("${hrp.url}")
-    private String hrpUrl;
+    @Autowired
+    UserService userService;
 
     @RequestMapping("/")
     public String index() {
@@ -31,11 +24,12 @@ public class UserController {
 
     @RequestMapping("/doLogin")
     public ResultApi login(HttpServletRequest request,
-                           String userName, String password) throws IOException {
-        UserInfo userInfo = checkUserPassword(userName, password);
-        if (userInfo == null) {
+                           String userName, String password) throws Exception {
+        String token = userService.getLoginToken(userName, password);
+        if (token == null) {
             return ResultApi.successInstance(false);
         }
+        UserInfo userInfo = userService.getUserInfo(userName, token);
         HttpSession session = request.getSession(true);
         session.setAttribute("userInfo", userInfo);
         return ResultApi.successInstance(true);
@@ -53,35 +47,5 @@ public class UserController {
         return ResultApi.successInstance(true);
     }
 
-    private UserInfo checkUserPassword(String userName, String password) throws IOException {
-        RestTemplate rt = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType("application/x-www-form-urlencoded; charset=UTF-8"));
-        Gson gson = new Gson();
-        LinkedMultiValueMap loginInfo = new LinkedMultiValueMap<String, String>();
-        loginInfo.add("CheckOnlineUser", "false");
-        loginInfo.add("op", "getTokenInfo");
-        loginInfo.add("USERNAME", userName);
-        loginInfo.add("PASSWORD", password);
-        loginInfo.add("SYSTEM", "KYEE");
-        String tokenJson = rt.postForObject(hrpUrl+"LoginHandler.jspx", new HttpEntity(loginInfo, headers), String.class);
-        ResultApi<String> token = gson.fromJson(tokenJson, ResultApi.class);
-        if (token == null || !token.getSuccess())
-            return null;
-        LinkedMultiValueMap tokenInfo = new LinkedMultiValueMap<String, String>();
-        tokenInfo.add("CheckOnlineUser", "false");
-        tokenInfo.add("op", "Query");
-        tokenInfo.add("token", token.getData());
-        tokenInfo.add("page", "1");
-        tokenInfo.add("rows", "1");
-        tokenInfo.add("obj", "{\"LOGIN_NAME\":\"" + userName + "\"}");
-        String userJson = rt.postForObject(hrpUrl+"UserPermSetting/UserPermSettingHandler.jspx", new HttpEntity(tokenInfo, headers), String.class);
-        HrpQueryResult user = gson.fromJson(userJson, HrpQueryResult.class);
-        if (user == null || user.getTotal() < 1) {
-            return null;
-        }
-        Map<String, String> map = user.getRows().get(0);
-        return new UserInfo(userName, map.get("USER_NAME"), map.get("DEPT_NAME"));
-    }
 
 }
---
+++ open-platform-backend/src/main/java/com/kyee/openplatform/service/UserService.java
@@ -0,0 +1,68 @@
+package com.kyee.openplatform.service;
+
+import com.google.gson.Gson;
+import com.kyee.openplatform.config.web.ResultApi;
+import com.kyee.openplatform.pojo.HrpQueryResult;
+import com.kyee.openplatform.repositorys.user.UserInfo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class UserService {
+
+    @Value("${hrp.url}")
+    private String hrpUrl;
+
+    private RestTemplate rt = new RestTemplate();
+    private HttpHeaders headers = new HttpHeaders();
+    private Gson gson = new Gson();
+
+    @PostConstruct
+    public void init() {
+        headers.setContentType(MediaType.parseMediaType("application/x-www-form-urlencoded; charset=UTF-8"));
+    }
+
+    public String getLoginToken(String userName, String password) throws IOException {
+        LinkedMultiValueMap loginInfo = new LinkedMultiValueMap<String, String>();
+        loginInfo.add("CheckOnlineUser", "false");
+        loginInfo.add("op", "getTokenInfo");
+        loginInfo.add("USERNAME", userName);
+        loginInfo.add("PASSWORD", password);
+        loginInfo.add("SYSTEM", "KYEE");
+        String tokenJson = rt.postForObject(hrpUrl + "LoginHandler.jspx", new HttpEntity(loginInfo, headers), String.class);
+        ResultApi<String> token = gson.fromJson(tokenJson, ResultApi.class);
+        if (token == null || !token.getSuccess())
+            return null;
+        return token.getData();
+    }
+
+    @Cacheable(value = "getUserInfo", key = "#userName")
+    public UserInfo getUserInfo(String userName, String token) throws Exception {
+        LinkedMultiValueMap tokenInfo = new LinkedMultiValueMap<String, String>();
+        tokenInfo.add("CheckOnlineUser", "false");
+        tokenInfo.add("op", "Query");
+        tokenInfo.add("token", token);
+        tokenInfo.add("page", "1");
+        tokenInfo.add("rows", "1");
+        tokenInfo.add("obj", "{\"LOGIN_NAME\":\"" + userName + "\"}");
+        String userJson = rt.postForObject(hrpUrl + "UserPermSetting/UserPermSettingHandler.jspx", new HttpEntity(tokenInfo, headers), String.class);
+        HrpQueryResult user = gson.fromJson(userJson, HrpQueryResult.class);
+        if (user == null || user.getTotal() < 1) {
+            throw new Exception("token未获取用户信息");
+        }
+        Map<String, String> map = user.getRows().get(0);
+        return new UserInfo(userName, map.get("USER_NAME"), map.get("DEPT_NAME"));
+    }
+
+
+}
